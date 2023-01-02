35df6f5942d1e18a208c4610ab6c0580daf954d4
AlexanderDLe <AlexanderLeDev@gmail.com>
2023-01-02 11:53:28 -0800

    2023 Start Leetcoding

--- Arrays&Hashing/ContainsDuplicate.py
+++ Arrays&Hashing/ContainsDuplicate.py
@@ -5,16 +5,25 @@
 '''
 
 
-class Solution:
+class SolutionSet:
   def containsDuplicate(self, nums):
     seen = set()
     for num in nums:
       if num in seen: return True
       seen.add(num)
     return False
+
+class SolutionSetAndLength:
+  def containsDuplicate(self, nums):
+    return len(set(nums)) != len(nums)
+
+class Solution:
+  def containsDuplicate(self, nums):
+    val = 0
   
 def runSolution():
   solution = Solution()
+  print(solution.containsDuplicate([3, 3, 1]))
   print(solution.containsDuplicate([1,2,3,1]))
   print(solution.containsDuplicate([1,2,3,4]))
   print(solution.containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
